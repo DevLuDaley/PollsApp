5e42c5ad132daed1a802156fff1e422af79c94b1
DevLuDaley <daleyluis@gmail.com>
2020-01-15 10:15:03 -0500

    +bug fix - import generic from django.views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.views import generic
+
 # from django.template import loader
 
 from .models import Choice, Question
